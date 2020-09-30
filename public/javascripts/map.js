5f0b97fe23dc5ba3e3102d2b16166a3c2a006b18
LukiMujica <lucas150998@gmail.com>
2020-09-30 00:41:36 -0300

    api de bicicletas

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,6 +4,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([-34.5825, -58.4343]).addTo(map);
-L.marker([-34.5650, -58.4273]).addTo(map);
-L.marker([-34.57882, -58.41579]).addTo(map);
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result) {
+        console.log(result);
+        result.bicicletas.forEach(function(bici) {
+            L.marker(bici.ubicacion, { title: bici.id }).addTo(map);
+        });
+    }
+})
