53347f9fdf797a2276bd21297e5a8d040707040e
LukiMujica <lucas150998@gmail.com>
2020-10-18 20:18:39 -0300

    Passport

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('./config/passport');
+const session = require('express-session');
+const jwt = require('jsonwebtoken');
+
 
 var indexRouter = require('./routes/index');
 var usuariosRouter = require('./routes/usuarios');
@@ -11,7 +15,19 @@ var bicicletasRouter = require('./routes/bicicletas');
 var bicicletasAPIRouter = require('./routes/api/bicicletas');
 var usuariosAPIRouter = require('./routes/api/usuarios');
 
+const store = new session.MemoryStore;
+
 var app = express();
+
+app.set('secretKey', 'jwt_pwd_!1223344');
+
+app.use(session({
+    cookie: { maxAge: 240 * 60 * 60 * 1000 },
+    store: store,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'red_bicis_123456'
+}));
 
 var mongoose = require('mongoose');
 
@@ -32,7 +48,78 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+app.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, usuario, info) {
+        if (err) return next(err);
+        if (!usuario) return res.render('login', { info });
+        req.login(usuario, function(err) {
+            if (err) return next(err);
+            return res.redirect('/');
+        });
+
+    })(req, res, next);
+});
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/login');
+});
+
+app.get('/forgotPassword', function(req, res) {
+    res.render('forgotPassword');
+});
+
+app.post('/forgotPassword', function(req, res) {
+    Usuario.findOne({ email: req.body.email }, function(err, usuario) {
+        if (!usuario) return res.render('forgotPassword', { info: { message: 'No existe el email para un usuario existente.' } });
+
+        usuario.resetPassword(function(err) {
+            if (err) return next(err);
+            console.log('forgotPasswordMessage');
+        });
+
+        res.render('forgotPasswordMessage');
+    });
+});
+
+app.get('/resetPassword/:token', function(req, res, next) {
+    Token.findOne({ token: req.params.token }, function(err, token) {
+        if (!token) return res.status(400).send({ type: 'not-verifified', msg: 'No existe un usuario asociado al token. Verifique que su token no haya expirado' });
+
+        Usuario.findById(token._userId, function(err, usuario) {
+            if (!usuario) return res.status(400).send({ msg: 'No existe un usuario asociado al token' });
+            res.render('resetPassword', { errors: {}, usuario: usuario });
+        });
+    });
+});
+
+app.post('/resetPassword', function(req, res) {
+    if (req.body.password != req.body.confirm_password) {
+        res.render('resetPassword', {
+            errors: { confirm_password: { message: 'No coincide con el password ingresado' } },
+            usuario: new Usuario({ email: req.body.email })
+        });
+        return;
+    }
+    Usuario.findOne({ email: req.body.email }, function(err, usuario) {
+        usuario.password = req.body.password;
+        usuario.save(function(err) {
+            if (err) {
+                res.render('resetPassword', { errors: err.errors, usuario: new Usuario({ email: req.body.email }) });
+            } else {
+                res.redirect('/login');
+            }
+        });
+    });
+});
 
 app.use('/', indexRouter);
 app.use('/usuarios', usuariosRouter);
---
+++ config/passport.js
@@ -0,0 +1,30 @@
+var passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Usuario = require('../models/usuario');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookTokenStrategy = require('passport-facebook-token');
+
+
+passport.use(new LocalStrategy(
+    function(email, password, done) {
+        Usuario.findOne({ email: email }, function(err, usuario) {
+            if (err) return done(err);
+            if (!usuario) return done(null, false, { message: 'Email no existente o incorrecto.' });
+            if (!usuario.validPassword(password)) return done(null, false, { message: 'Password incorrecto.' });
+            return done(null, usuario);
+        });
+    }
+));
+
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+    Usuario.findById(id, function(err, usuario) {
+        cb(err, usuario);
+    });
+});
+
+module.exports = passport;
