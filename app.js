7edd349a238856d761c86435409a9479a15bb17f
LukiMujica <lucas150998@gmail.com>
2020-10-23 14:34:22 -0300

    Agregamos Politica de Privacidad

--- app.js
+++ app.js
@@ -136,6 +136,11 @@ app.use('/bicicletas', loggedIn, bicicletasRouter);
 app.use('/api/bicicletas', validarUsuario, bicicletasAPIRouter);
 app.use('/api/usuarios', usuariosAPIRouter);
 
+app.use('/privacy_policy', (req, res) => {
+    res.sendFile('public/privacy_policy.html');
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
