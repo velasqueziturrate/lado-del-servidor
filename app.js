507c86de9a86bf000abe50c1b9a679735a85178f
LukiMujica <lucas150998@gmail.com>
2020-10-23 17:49:47 -0300

    Agregamos FacebookOAuth

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ app.get('/auth/google',
     passport.authenticate('google', {
         scope: [
             'https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read',
-            'profile', 'email '
+            'profile', 'email ', 'password'
         ]
     }));
 
