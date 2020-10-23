9c82187b47fe2b5fd29975c7eb2f50648de80bd6
LukiMujica <lucas150998@gmail.com>
2020-10-23 15:34:12 -0300

    Agregamos Google OAuth

--- app.js
+++ app.js
@@ -158,15 +158,7 @@ app.use('/googlebe316eb8954df9f9', (req, res) => {
     res.sendFile('public/googlebe316eb8954df9f9.html');
 });
 
-app.get('/auth/google',
-    passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    function(req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    });
 
 
 // catch 404 and forward to error handler
--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const Usuario = require('../models/usuario');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookTokenStrategy = require('passport-facebook-token');
+
 
 
 passport.use(new LocalStrategy(
@@ -29,23 +29,6 @@ passport.use(new GoogleStrategy({
         });
     }
 ));
-
-passport.use(new FacebookTokenStrategy({
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: process.env.HOST + "/auth/facebook/callback"
-}, function(accessToken, refreshToken, profile, done) {
-    try {
-        Usuario.findOneOrCreateByFacebook(profile, function(err, user) {
-            if (err) console.log('err' + err);
-            return done(err, user);
-        });
-    } catch (err2) {
-        console.log(err2);
-        return done(err2, null);
-    }
-}));
-
 
 passport.serializeUser(function(user, cb) {
     cb(null, user.id);
